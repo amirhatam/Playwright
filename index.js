0fef4307ecd3976b5d2b26688c5f21a5fd188481
Amir Hatam <64803746+amirhatam@users.noreply.github.com>
2022-12-21 16:35:41 +0100

    Update and rename README to index.js
    modifié

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
+const { test, expect } = require("@playwright/test");
 const { chromium } = require('playwright');
 
-(async () => {
-  const browser = await chromium.launch({
-    headless: false
-  });
-  const context = await browser.newContext();
+test("Calculate 10 * 5 should return 50", async ({ page }) => {
+  await page.goto("https://www.desmos.com/scientific?lang=fr/");
+  
+  
   await page.getByRole('button', { name: '1' }).click();
   await page.getByRole('button', { name: '0' }).click();
   await page.getByRole('button', { name: 'Multiplier' }).click();
   await page.getByRole('button', { name: '5' }).click();
   await page.getByRole('group', { name: 'clavier' }).getByRole('button', { name: 'Entrée' }).click();
 
-  // ---------------------
-  await context.close();
-  await browser.close();
-})();
+ await expect(page.getByText("equals 2=2")).toContainText("2");
+});
