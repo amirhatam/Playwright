a8de2d7318e349349487b40282ed0dfdfba26d18
Amir Hatam <64803746+amirhatam@users.noreply.github.com>
2022-12-21 17:08:25 +0100

    Update index.js
    last modified

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const { test, expect } = require("@playwright/test");
-const { chromium } = require('playwright');
 
-test("Calculate 10 * 5 should return 50", async ({ page }) => {
+
+test("Calculate 5 * 5 should return 50", async ({ page }) => {
   await page.goto("https://www.desmos.com/scientific?lang=fr/");
   
-  
-  await page.getByRole('button', { name: '1' }).click();
-  await page.getByRole('button', { name: '0' }).click();
+  await page.getByRole('button', { name: '5' }).click();
   await page.getByRole('button', { name: 'Multiplier' }).click();
   await page.getByRole('button', { name: '5' }).click();
   await page.getByRole('group', { name: 'clavier' }).getByRole('button', { name: 'Entrée' }).click();
 
- await expect(page.getByText("equals 2=2")).toContainText("2");
+ await expect(page.getByText("equals =50")).toContainText("=50");
 });
